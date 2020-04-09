7e020d736178603379a77c1446d570b48eb2455f
djclayton21 <clayton.daniel.j@gmail.com>
2020-04-09 13:31:01 -0600

    filtering, pagination, sorting

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,28 @@
-function feed(parent, args, context, info) {
-  return context.prisma.links();
+async function feed(parent, args, context, info) {
+  const where = args.filter
+    ? {
+        OR: [
+          { description_contains: args.filter },
+          { url_contains: args.filter },
+        ],
+      }
+    : {};
+  const links = await context.prisma.links({
+    where,
+    first: args.first,
+    skip: args.skip,
+    orderBy: args.orderBy,
+  });
+  const count = await context.prisma
+    .linksConnection({
+      where,
+    })
+    .aggregate()
+    .count();
+  return {
+    links,
+    count,
+  };
 }
 
 function link(parent, args, context, info) {
