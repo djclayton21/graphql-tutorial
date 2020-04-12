8de161627609e385ba259a3a674adfbb77fcd3d5
djclayton21 <clayton.daniel.j@gmail.com>
2020-04-12 16:38:30 -0600

    connect feed query and create link

---
+++ client/src/componenets/CreateLink.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { Mutation } from 'react-apollo';
+import { gql } from 'apollo-boost'
+
+const POST_MUTATION = gql`
+  mutation PostMutation($description: String!, $url: String!) {
+    postLink(description: $description, url: $url) {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`;
+
+export default function CreateLink() {
+  const [url, setUrl] = useState('');
+  const [description, setDescription] = useState('');
+  return (
+    <div>
+      <div className="flex flex-column mt3">
+        <input
+          className="mb2"
+          value={description}
+          onChange={event => setDescription(event.target.value)}
+          type="text"
+          placeholder="Description"
+        />
+        <input
+          className="mb2"
+          value={url}
+          onChange={event => setUrl(event.target.value)}
+          type="text"
+          placeholder="url"
+        />
+        <Mutation mutation={POST_MUTATION} variables={{ description, url }}>
+          {postMutation => <button onClick={postMutation}>Submit</button>}
+        </Mutation>
+      </div>
+    </div>
+  );
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import App from './components/App';
+import App from './componenets/App';
 import * as serviceWorker from './serviceWorker';
 
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:7890'
 });
 
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjazh1ZDQ4OWV4Mnd0MDkzNHEwNWwyaGFzIiwiaWF0IjoxNTg2NzMwODE0fQ.9wyImezDOvBCendXy4fw8-pMQhHjzAYDi_o9YH-S-k4';
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
